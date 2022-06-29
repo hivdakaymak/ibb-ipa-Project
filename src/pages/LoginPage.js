7a41fcdb76254706083378917b856b203f66fb6f
miraykaymak <miirayazz00@gmail.com>
2022-06-29 11:40:07 +0300

    updated

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Menu from "../compenents/Navbar";
+
 import { Container, Row, Form, Button, Col } from "react-bootstrap";
-import Navbars from "../compenents/Navbar";
 import UsersData from "./users.json";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Formik } from "formik";
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("Lütfen adınızı yazınız"),
+  lastName: Yup.string().required("Lütfen şifrenizi giriniz"),
+});
+
 const LoginPage = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -14,95 +22,69 @@ const LoginPage = () => {
 
   return (
     <Container>
+      <Menu title="Gİrİş Sayfası" />
       <Row>
-        <Navbars />
-        <Col md={12}>
-          <div className="login-page-form d-flex align-items-center justify-content-center">
-            <Formik
-              initialValues={{ name: "", password: "" }}
-              validate={(values) => {
-                const errors = {};
-                if (!values.name) {
-                  errors.name = "Required";
-                }
-                if(!values.password){
-                  errors.password = "sdfönsdf"
-                }
-                return errors;
-              }}
-              onSubmit={(values, { setSubmitting }) => {
-                console.log(values);
-                const userControl = UsersData.users.find(
-                  (data) => data.name === values.name
-                );
-                if (userControl) {
-                  if (userControl.password === values.password) {
-                    localStorage.setItem(
-                      "user-data",
-                      JSON.stringify(userControl)
-                    );
-                    navigate("/events-var");
-                  }
-                }
-                console.log(UsersData.users);
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-              }) => (
-                <Form
-                  onSubmit={(e) => {
-                    handleSubmit(e);
-                    e.preventDefault();
-                  }}
-                  style={{ width: "70%", marginTop: "50px" }}
-                >
-                  <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Adınız</Form.Label>
-                    <Form.Control
-                      onChange={handleChange}
-                      type="text"
-                      placeholder="Enter name"
-                      onBlur={handleBlur}
-                    />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    {errors.name && touched.name && errors.name}
-                    <Form.Control
-                      onChange={handleChange}
-                      type="password"
-                      placeholder="Password"
-                      onBlur={handleBlur}
-                    />
-                    {errors.password && touched.password && errors.password}
-                  </Form.Group>
+        <Formik
+          initialValues={{ firstName: "", lastName: "" }}
+          validationSchema={validationSchema}
+          // onSubmit={(e) => {
+          //   handleSubmit(e);
+          //   e.preventDefault();
+          // }}
 
-                  <button
-                    variant="primary"
-                    type="submit"
-                    disabled={isSubmitting}
-                    style={{
-                      width: "15%",
-                      border: "none",
-                      background: "#0dcaf0",
-                      padding: "10px",
-                      color: "white",
-                    }}
-                  >
-                    Giriş Yap
-                  </button>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        </Col>
+          onSubmit={(values, { setSubmitting }) => {
+         
+            console.log(values);
+            const userControl = UsersData.users.find(
+              (data) => data.name === values.name
+            );
+            if (userControl) {
+              if (userControl.password === values.password) {
+                localStorage.setItem("user-data", JSON.stringify(userControl));
+                navigate("/events-var");
+              }
+            }
+            console.log(UsersData.users);
+          }}
+        >
+          {({ handleSubmit, handleChange, values, errors, isSubmitting }) => (
+            //
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Ad"
+                onChange={handleChange}
+                values={values.firstName}
+              />
+              {errors.firstName && errors.firstName}
+
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Şifre"
+                onChange={handleChange}
+                values={values.lastName}
+              />
+              {errors.lastName && errors.lastName}
+
+              <button
+                variant="primary"
+                type="submit"
+                disabled={isSubmitting}
+                style={{
+                  width: "15%",
+                  border: "none",
+                  background: "#0dcaf0",
+                  padding: "10px",
+                  color: "white",
+                }}
+              >
+                Giriş Yap
+              </button>
+            </form>
+          )}
+        </Formik>
       </Row>
     </Container>
   );
