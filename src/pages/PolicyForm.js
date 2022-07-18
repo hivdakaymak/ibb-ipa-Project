64254dea75556312eb55b02341af84b17a876601
miraykaymak <miirayazz00@gmail.com>
2022-07-18 14:27:20 +0300

    formik yup updated

--- src/pages/PolicyForm.js
+++ src/pages/PolicyForm.js
@@ -42,14 +42,16 @@ const policyformSchema = Yup.object().shape({
     .required("Lütfen İstanbul Kart Numaranız doldurunuz"),
 });
 
-const formButton = () => {
-  Swaler.fire({
-    icon: "success",
-    title: "Kaydedildi",
-    text: "Başvurunuz kaydedildi",
-    timer: 1500,
-  });
-};
+// Kaydedildi Uyarısı
+
+// const formButton = () => {
+//   Swaler.fire({
+//     icon: "success",
+//     title: "Kaydedildi",
+//     text: "Başvurunuz kaydedildi",
+//     timer: 1500,
+//   });
+// };
 const initialValues = {
   email: "",
   password: "",
@@ -61,128 +63,169 @@ const initialValues = {
   cardNo: "",
 };
 
+const validate = (values) => {
+  let errors = {};
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  if (!values.email) {
+    errors.email = "Lütfen E-mail adresinizi giriniz";
+  } else if (!regex.test(values.email)) {
+    errors.email = "Geçersiz E-mail";
+  }
+  if (!values.password) {
+    errors.password = "Lütfen şifrenizi giriniz";
+  } else if (values.password.length < 4) {
+    errors.password = "Şifreniz en az 4 karakterden oluşmalıdır";
+  }
+  return errors;
+};
+
+const submitForm = (values) => {
+  console.log(values);
+};
+
 const PolicyForm = () => {
   return (
-    <Formik
-      initialValues={initialValues}
-      validate={validate}
-      onSubmit={submitForm}
-    >
+    <>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={submitForm}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty,
+        }) => (
+          <Container>
+            <Row>
+              <Menu title="SOSYAL POLİTİKALAR ÇALIŞTAYI BAŞVURU FORMU" />
 
-      
-      <Container>
-        <Row>
-          <Menu title="SOSYAL POLİTİKALAR ÇALIŞTAYI BAŞVURU FORMU" />
-
-          <Col md={9} className="policyForm mt-5">
-            <Form className="">
-              <Form.Group
-                className="mt-3 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className=""> Ad</Form.Label>
-                <Form.Control
-                  className="login-form-input "
-                  name="firstName"
-                  type="text"
-                />
-              </Form.Group>
-              <Form.Group
-                className="mt-3 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className="policyText"> Soyad</Form.Label>
-                <Form.Control
-                  className="login-form-input "
-                  name="lastName"
-                  type="text"
-                />
-              </Form.Group>
-              <Form.Group
-                className="mt-3 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className="policyText"> TCKN</Form.Label>
-                <Form.Control
-                  className="login-form-input "
-                  name="idNo"
-                  type="text"
-                />
-              </Form.Group>
-              <Form.Group
-                className="mt-3 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className="policyText"> E-Posta</Form.Label>
-                <Form.Control
-                  className="login-form-input "
-                  name="mail"
-                  type="text"
-                />
-              </Form.Group>
-              <Form.Group
-                className="mt-3 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className="policyText"> Telefon</Form.Label>
-                <Form.Control
-                  className="login-form-input "
-                  name="phoneNumber"
-                  type="text"
-                />
-              </Form.Group>
-              <Form.Group
-                className="mt-3 mb-5 d-flex align-items-center justify-content-between"
-                controlId="formBasicEmail"
-              >
-                <Form.Label className="policyText">
-                  {" "}
-                  İstanbul Kart No
-                </Form.Label>
-                <InputGroup className="login-form-input ">
-                  <Form.Control
-                    className=" none-border-r"
-                    name="cardNo"
-                    type="text"
-                  />
-                  <div className="input-group-prepend  ">
-                    <div class="input-group-text none-border-l  bg-transparent p-0 border-left-0">
-                      <Modaler />
-                    </div>
+              <Col md={9} className="policyForm mt-5">
+                <Form className="" onSubmit={handleSubmit}>
+                  <Form.Group
+                    className="mt-3 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className=""> Ad</Form.Label>
+                    <Form.Control
+                      className="login-form-input "
+                      name="firstName"
+                      type="text"
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mt-3 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="policyText"> Soyad</Form.Label>
+                    <Form.Control
+                      className="login-form-input "
+                      name="lastName"
+                      type="text"
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mt-3 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="policyText"> TCKN</Form.Label>
+                    <Form.Control
+                      className="login-form-input "
+                      name="idNo"
+                      type="text"
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mt-3 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="policyText"> E-Posta</Form.Label>
+                    <Form.Control
+                      // className="login-form-input "
+                      name="email"
+                      type="text"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={ 
+                        errors.email && touched.email ? "input-error login-form-input" : null
+                      }
+                    />
+                    {errors.email && touched.email && (
+                      <span className="error">{errors.email}</span>
+                    )}
+                    
+                  </Form.Group>
+                  <Form.Group
+                    className="mt-3 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="policyText"> Telefon</Form.Label>
+                    <Form.Control
+                      className="login-form-input "
+                      name="phoneNumber"
+                      type="text"
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mt-3 mb-5 d-flex align-items-center justify-content-between"
+                    controlId="formBasicEmail"
+                  >
+                    <Form.Label className="policyText">
+                      {" "}
+                      İstanbul Kart No
+                    </Form.Label>
+                    <InputGroup className="login-form-input ">
+                      <Form.Control
+                        className=" none-border-r"
+                        name="cardNo"
+                        type="text"
+                      />
+                      <div className="input-group-prepend  ">
+                        <div class="input-group-text none-border-l  bg-transparent p-0 border-left-0">
+                          <Modaler />
+                        </div>
+                      </div>
+                    </InputGroup>
+                  </Form.Group>
+                  <div className="d-flex justify-content-center">
+                    <Form.Group
+                      className="mb-3 policy-check"
+                      controlId="formBasicCheckbox"
+                    >
+                      <Form.Check type="checkbox" label="Açık Rıza Metni" />
+                    </Form.Group>
+                    <Form.Group
+                      className="mb-3 policy-check"
+                      controlId="formBasicCheckbox"
+                    >
+                      <Form.Check type="checkbox" label="KVKK" />
+                    </Form.Group>
                   </div>
-                </InputGroup>
-              </Form.Group>
-              <div className="d-flex justify-content-center">
-                <Form.Group
-                  className="mb-3 policy-check"
-                  controlId="formBasicCheckbox"
-                >
-                  <Form.Check type="checkbox" label="Açık Rıza Metni" />
-                </Form.Group>
-                <Form.Group
-                  className="mb-3 policy-check"
-                  controlId="formBasicCheckbox"
-                >
-                  <Form.Check type="checkbox" label="KVKK" />
-                </Form.Group>
-              </div>
-              <Col
-                md={12}
-                className="d-flex align-items-cengter justify-content-center policyFormButton mb-5"
-              >
-                <Button
-                  onClick={formButton}
-                  className="login-save-button mb- mt-5"
-                  variant="info"
-                >
-                  Kaydet
-                </Button>
+                  <Col
+                    md={12}
+                    className="d-flex align-items-cengter justify-content-center policyFormButton mb-5"
+                  >
+                    <Button
+                      // onClick={formButton}
+                      className="login-save-button mb- mt-5"
+                      variant="info"
+                    >
+                      Kaydet
+                    </Button>
+                  </Col>
+                </Form>
               </Col>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </Formik>
+            </Row>
+          </Container>
+        )}
+      </Formik>
+    </>
   );
 };
 
